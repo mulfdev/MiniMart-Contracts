0a2560ea92e07a1ece6fac0d80f35df10028cc5d
mulf <mail@mulf.wtf>
2025-06-06 12:24:02 -0500

    update bps

--- src/MiniMart.sol
+++ src/MiniMart.sol
@@ -6,7 +6,7 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
 contract MiniMart is Ownable {
-    uint8 public constant FEE_BPS = 15;
+    uint8 public constant FEE_BPS = 150;
     address public feeRecipient;
 
     event FeeRecipientUpdated(address indexed newRecipient);
