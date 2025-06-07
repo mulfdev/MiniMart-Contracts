2826cbe497efb35600c2d3677e982d35c1dd5e78
mulf <mail@mulf.wtf>
2025-06-07 16:20:39 -0500

    improve deploy local script

--- script/DeployLocal.s.sol
+++ script/DeployLocal.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {Script, console} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 import {MiniMart} from "../src/MiniMart.sol";
 import {TestNFT} from "../src/TestNFT.sol";
 
@@ -13,12 +14,13 @@ contract DeployLocal is Script {
     function run() public {
         vm.createSelectFork("local");
 
-        // DONT PANICK, this is an avil provided private key
+        // DONT PANICK, this is an anvil provided private key
         vm.startBroadcast(
             0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
         );
 
-        address wallet = vm.envAddress("WALLET_ADDR");
+        // anvil provided wallet
+        address wallet = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
 
         (bool success, ) = wallet.call{value: 0.1 ether}("");
         require(success, "Failed to send Ether");
@@ -31,6 +33,19 @@ contract DeployLocal is Script {
 
         nft.mint(wallet);
         nft.mint(wallet);
+
+        bytes32 newOrderId = marketplace.addOrder(
+            MiniMart.Order({
+                price: 0.1 ether,
+                nftContract: address(nft),
+                tokenId: 1,
+                seller: msg.sender,
+                expiration: 0
+            })
+        );
+
+        console.log("New order sumbitted. Order id: ");
+        console.logBytes32(newOrderId);
 
         vm.stopBroadcast();
     }
