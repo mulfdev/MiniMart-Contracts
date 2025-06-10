a3290ab6e0d1d47a74b9d26dcbc17c24c5caad0e
mulf <mail@mulf.wtf>
2025-06-10 18:05:17 -0500

    ts and eslint stuff

--- utils/eslint.config.js
+++ utils/eslint.config.js
@@ -5,7 +5,7 @@ import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
     eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
     prettierConfig,
 
     {
