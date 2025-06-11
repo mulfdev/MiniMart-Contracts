17235c95c3a31e0b3a38770170b51a99ed1d8f01
mulf <mail@mulf.wtf>
2025-06-11 13:30:17 -0500

    fmt

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Script, console} from "forge-std/Script.sol";
-import {MiniMart} from "../src/MiniMart.sol";
+import { Script, console } from "forge-std/Script.sol";
+import { MiniMart } from "../src/MiniMart.sol";
 
 contract CounterScript is Script {
-    // MiniMart public marketplace;
-    //
-    // function setUp() public { }
-    //
-    // function run() public {
-    //     vm.createSelectFork("base");
-    //     vm.startBroadcast();
-    //
-    //     marketplace = new MiniMart(payable(vm.envAddress("WALLET_ADDR")));
-    //
-    //     vm.stopBroadcast();
-    // }
+// MiniMart public marketplace;
+//
+// function setUp() public { }
+//
+// function run() public {
+//     vm.createSelectFork("base");
+//     vm.startBroadcast();
+//
+//     marketplace = new MiniMart(payable(vm.envAddress("WALLET_ADDR")));
+//
+//     vm.stopBroadcast();
+// }
 }
