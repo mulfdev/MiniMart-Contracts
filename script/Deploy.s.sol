e8173988fe2450e8ff775e6db40b2340110da07a
mulf <mail@mulf.wtf>
2025-06-17 17:41:40 -0500

    use create3-factory to deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,21 +1,36 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.30;
 
-import { Script, console } from "forge-std/Script.sol";
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import { ICREATE3Factory } from "create3-factory/src/ICREATE3Factory.sol";
 import { MiniMart } from "../src/MiniMart.sol";
-import { CREATE3 } from "solady/utils/CREATE3.sol";
 
 contract Deploy is Script {
-// MiniMart public marketplace;
-//
-// function setUp() public { }
-//
-// function run() public {
-//     vm.createSelectFork("base");
-//     vm.startBroadcast();
-//
-//     marketplace = new MiniMart(payable(vm.envAddress("WALLET_ADDR")));
-//
-//     vm.stopBroadcast();
-// }
+    ICREATE3Factory constant FACTORY = ICREATE3Factory(0x9fBB3DF7C40Da2e5A0dE984fFE2CCB7C47cd0ABf);
+
+    function run() external {
+        vm.createSelectFork("base_sepolia");
+
+        bytes32 salt = keccak256("minimartv1.0_mulf-deployer");
+        address eoaDeployer = msg.sender;
+
+        console.log("Deployer (EOA):", eoaDeployer);
+
+        bytes memory initCode =
+            abi.encodePacked(type(MiniMart).creationCode, abi.encode(eoaDeployer, "MiniMart", "1"));
+
+        address predicted = FACTORY.getDeployed(eoaDeployer, salt);
+        console.log("Predicted Address:", predicted);
+
+        vm.startBroadcast(eoaDeployer);
+
+        address deployed = FACTORY.deploy(salt, initCode);
+
+        vm.stopBroadcast();
+
+        console.log("Deployed Address: ", deployed);
+        require(deployed == predicted, "Address Mismatch: The final check failed.");
+        console.log("Success! MiniMart deployed to:", deployed);
+    }
 }
