81a0ae35216f1211c6af5ff740d80ee2a94c8881
mulf <mail@mulf.wtf>
2025-06-17 14:13:19 -0500

    pin sol version

--- script/DeployLocal.s.sol
+++ script/DeployLocal.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.30;
 
 import { Script, console } from "forge-std/Script.sol";
 import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
