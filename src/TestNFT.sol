67e5e7e726085e29b6958f987a8f00248857ea1e
mulf <mail@mulf.wtf>
2025-06-17 14:13:06 -0500

    update test nft

--- src/TestNFT.sol
+++ src/TestNFT.sol
@@ -1,31 +1,27 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.30;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TestNFT is ERC721, Ownable {
-    string private _fixedTokenURI;
+    string private fixedTokenURI;
     uint256 private _nextTokenId;
 
     constructor(string memory fixedURI, address initialOwner)
         ERC721("TestNFT", "TNFT")
         Ownable(initialOwner)
     {
-        _fixedTokenURI = fixedURI;
+        fixedTokenURI = fixedURI;
     }
 
-    function mint(address user) public {
-        uint256 tokenId = _nextTokenId++;
-        _mint(user, tokenId);
+    function mint(address to) external {
+        uint256 id = _nextTokenId++;
+        _safeMint(to, id);
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         _requireOwned(tokenId);
-        return _fixedTokenURI;
-    }
-
-    function setFixedURI(string memory newURI) external onlyOwner {
-        _fixedTokenURI = newURI;
+        return fixedTokenURI;
     }
 }
