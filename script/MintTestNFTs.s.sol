18c0d5331acb6b3982c36af36f811aed3c34fa63
mulf <mail@mulf.wtf>
2025-06-17 18:39:07 -0500

    revert constructor changes

--- script/MintTestNFTs.s.sol
+++ script/MintTestNFTs.s.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.30;
 
 import "forge-std/Script.sol";
 import "forge-std/console2.sol";
-import { CREATE3 } from "solady/utils/CREATE3.sol";
 import { MiniMart } from "../src/MiniMart.sol";
 import { TestNFT } from "../src/TestNFT.sol";
 
@@ -11,7 +10,7 @@ contract Deploy is Script {
     function run() external {
         vm.createSelectFork("base_sepolia");
 
-        TestNFT testNft = new TestNFT("https://media.mulf.wtf/testnft-img.png");
+        TestNFT testNft = new TestNFT("https://media.mulf.wtf/testnft-img.png", msg.sender);
         for (uint8 i; i < 15; i++) {
             testNft.mint(0x02F9B04A37b089b5887c491097E62D2111c2BB7F);
         }
